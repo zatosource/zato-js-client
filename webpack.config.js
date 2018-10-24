691e061398f6bc42adb8d9365e3c357d3b31c951
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-10-24 22:31:45 +0200

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/*
+Copyright (C) 2018, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+*/
+
+module.exports = {
+  entry: "./src/zato-client.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  mode: "production",
+  module: {},
+  externals: {}
+}
